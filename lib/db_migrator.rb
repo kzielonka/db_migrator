2a937cc4cbc24acfdd74541e2dbdfd3e58ac946f
Krzysztof Zielonka <aknolezik@gmail.com>
2019-01-04 17:52:24 +0100

    Add basic migration implementation

--- lib/db_migrator.rb
+++ lib/db_migrator.rb
@@ -1,5 +1,19 @@
 class DBMigrator
+  def initialize(db)
+    @db = db
+    @migrations = []
+  end
+
   def migrate
-    # loads migrations and applies them on db
+    @db.connect
+    @migrations.each_with_index do |m, i|
+      next unless @db.exec("INSERT INTO migrations (id) VALUES '#{i + 1}';") == :success
+      @db.exec(m)
+    end
+    @db.close_connection
+  end
+
+  def add_migration(migration)
+    @migrations << String(migration)
   end
 end
--- spec/db_migrator_spec.rb
+++ spec/db_migrator_spec.rb
@@ -1,11 +1,50 @@
 require "spec_helper"
 
 describe DBMigrator do
-  subject(:migrator) { DBMigrator.new }
+  subject(:migrator) { DBMigrator.new(db) }
+
+  let(:db) do
+    instance_double(
+      "db",
+      connect: :success,
+      close_connection: :success,
+      exec: :success,
+    )
+  end
 
   it "doesn't raise any error calling #migrate" do
     expect do
       migrator.migrate
     end.not_to raise_exception
+  end
+
+  it "doesn't raise any error adding migration" do
+    expect do
+      migrator.add_migration("migration")
+    end.not_to raise_exception
+  end
+
+  it "applies added migrations on DB" do
+    migrator.add_migration("migration 1")
+    migrator.add_migration("migration 2")
+    migrator.migrate
+
+    expect(db).to have_received(:exec).with("INSERT INTO migrations (id) VALUES '1';")
+    expect(db).to have_received(:exec).with("migration 1")
+    expect(db).to have_received(:exec).with("INSERT INTO migrations (id) VALUES '2';")
+    expect(db).to have_received(:exec).with("migration 2")
+  end
+
+  it "invokes only not created migrations" do
+    allow(db).to receive(:exec).with("INSERT INTO migrations (id) VALUES '1';").and_return :record_already_exists
+
+    migrator.add_migration("migration 1")
+    migrator.add_migration("migration 2")
+    migrator.migrate
+
+    expect(db).to have_received(:exec).with("INSERT INTO migrations (id) VALUES '1';")
+    expect(db).not_to have_received(:exec).with("migration 1")
+    expect(db).to have_received(:exec).with("INSERT INTO migrations (id) VALUES '2';")
+    expect(db).to have_received(:exec).with("migration 2")
   end
 end
